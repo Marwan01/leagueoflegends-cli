9fbbcce5885c52dbfbb6b6b116d1dbc4a7bc3f0f
Marwan01 <marwanest@live.fr>
2020-01-22 10:40:38 -0600

    added counter getter function with champ name or number

--- championgetter.js
+++ championgetter.js
@@ -3,22 +3,17 @@ const puppeteer = require('puppeteer');
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.goto('https://na.leagueoflegends.com/en/game-info/champions/');
+  await page.goto('https://na.leagueoflegends.com/en-us/champions/');
 
   var champions = await page.evaluate(() => {
-    var tds = Array.from(document.querySelectorAll(' li .champ-name a'))
+    var tds = Array.from(document.querySelectorAll(' .style__Text-sc-12h96bu-3'))
     return tds.map(li => li.innerHTML)
   });
 
   let i=0;
   champions.forEach(champ => {
-      if(champ.includes(" ") || champ.includes("\'")){
-          champ = champ.replace(' ', '')
-          champ = champ.replace('\'', '')
-          champ = champ.replace('&amp; ', '')
-      }
-
       console.log(i++ + ") " + champ)
   })
+  
   await browser.close();
 })();
--- countergetter.js
+++ countergetter.js
@@ -1,17 +1,37 @@
 const puppeteer = require('puppeteer');
 
+let userChamp = process.argv[2];
+
+
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.goto('https://na.leagueoflegends.com/en/game-info/champions/');
+  await page.goto('https://na.leagueoflegends.com/en-us/champions/');
 
-  const champions = await page.evaluate(() => {
-    const tds = Array.from(document.querySelectorAll(' li .champ-name a'))
+  var champions = await page.evaluate(() => {
+    var tds = Array.from(document.querySelectorAll(' .style__Text-sc-12h96bu-3'))
     return tds.map(li => li.innerHTML)
   });
 
-  const page = await browser.newPage();
-  await page.goto('https://na.leagueoflegends.com/en/game-info/champions/');
+  let i=0;
+  champions.forEach(champ => {
+      if(champ.includes(" ") || champ.includes("\'")){
+          champ = champ.replace(' ', '')
+          champ = champ.replace('\'', '')
+          champ = champ.replace('&amp; ', '')
+      }
+  })
+  if(Number.isInteger(parseInt(userChamp)) ) {
+    await page.goto('https://lolcounter.com/champions/' + champions[userChamp]);
+  } else {
+    await page.goto('https://lolcounter.com/champions/' + userChamp);
+  }
   
+  var counters = await page.evaluate(() => {
+    var tds = Array.from(document.querySelectorAll(' .weak-block .name'))
+    return tds.map(li => li.innerHTML)
+  });
+  counters = counters.slice(0,10)
+  console.log(counters)
   await browser.close();
 })();
